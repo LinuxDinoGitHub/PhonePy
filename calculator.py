59d08a65443374bd4ff091898ab088f6c3a1ea43
LinuxDinoGitHub <linusthedinosaur@gmail.com>
2024-07-14 17:40:17 +0800

    UI finished

--- calculator.py
+++ calculator.py
@@ -11,16 +11,16 @@ buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
 buttonFrame.columnconfigure(3, weight=1)
 
-buttonText = [1,2,3,"+",4,5,6,"-",7,8,9,0]
+buttonText = [1,2,3,"+",4,5,6,"-",7,8,9,"=",0,"x","/"]
 btn = {}
 
 for i in buttonText:
     btn[i] = Button(buttonFrame, text=str(i), font=('Arial',38))
 
 for i ,v in enumerate(btn.values()):
-    v.grid(row=(i-1)%4,column=(i-(i%3)))
+    v.grid(column=(i)%4,row=(i-(i%4)))
 #123456789
 #000111222
-buttonFrame.pack()
+buttonFrame.pack(fill='x')
 
 root.mainloop()
